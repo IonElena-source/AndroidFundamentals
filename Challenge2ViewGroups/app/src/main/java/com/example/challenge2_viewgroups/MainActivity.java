401ec75a8754271d67a7530c62b21904fbee2743
Cristina <ionelenacristina0@gmail.com>
2020-12-07 21:26:14 +0200

    ViewGroups

---
+++ Challenge2ViewGroups/app/src/main/java/com/example/challenge2_viewgroups/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.challenge2_viewgroups;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
